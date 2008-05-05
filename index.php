5c0d6ca9f46130e244e273e525c83476ee4474d1
aoverholt <aoverholt>
2008-05-05 21:11:46 +0000

    Close <div> and add _projectCommon.php.

--- index.php
+++ index.php
@@ -48,6 +48,7 @@
 	   <h6>Incubation</h6>
 	   <div style="text-align: center">
 	   <a href="/projects/what-is-incubation.php"><img src="/images/egg-incubation.png" alt="Incubation"/></a>
+	   </div>
 	</div>
 
 EOHTML;
