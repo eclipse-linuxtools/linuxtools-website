5a3d91a0226cbf8213c6f51c02d4d02de5ca6909
aoverholt <aoverholt>
2008-12-18 20:26:44 +0000

    Clean up nav bar.

--- _projectCommon.php
+++ _projectCommon.php
@@ -26,20 +26,20 @@ $pageKeywords = isset($pageKeywords) ? $pageKeywords : "linux, eclipse";
 
 # Page-specific Nav bars go here
 # Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
-#$Nav->addNavSeparator("Linuxtools home", 	"index.php");
-$Nav->addCustomNav("About This Project", "/projects/project_summary.php?projectid=technology.linux-distros", "", 1);
-$Nav->addCustomNav("Wiki",				"http://wiki.eclipse.org/index.php/Linux_Distributions_Project", "_self", 2);
-$Nav->addCustomNav("Plan", "http://www.eclipse.org/projects/project-plan.php?projectid=technology.linux-distros", "", 1);
-#$Nav->addCustomNav("Developers", 		"developers.php", 		"_self", 2);
-$Nav->addCustomNav("Downloads", 	"downloads.php");
-$Nav->addNavSeparator("Projects", 	"index.php");
-	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;Autotools", "/linuxtools/projectPages/autotools", "_self", 1);
-	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;ChangeLog", "/linuxtools/projectPages/changelog", "_self", 1);
-	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;eclipse-build", "/linuxtools/projectPages/eclipse-build", "_self", 1);
-	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;OProfile", "/linuxtools/projectPages/oprofile", "_self", 1);
-	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;RPM Stubby", "/linuxtools/projectPages/rpmstubby", "_self", 1);
-	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;Specfile Editor", "/linuxtools/projectPages/specfile", "_self", 1);
-	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;Systemtap", "/linuxtools/projectPages/systemtap", "_self", 1);
-	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;Valgrind", "/linuxtools/projectPages/valgrind", "_self", 1);
+$Nav->addNavSeparator("Linux Tools", "index.php");
+    $Nav->addCustomNav("Project Summary", "/projects/project_summary.php?projectid=technology.linux-distros", "", 1);
+    $Nav->addCustomNav("Plan", "http://www.eclipse.org/projects/project-plan.php?projectid=technology.linux-distros", "", 2);
+    $Nav->addCustomNav("Wiki", "http://wiki.eclipse.org/index.php/Linux_Distributions_Project", "_self", 2);
+    $Nav->addCustomNav("Get Involved", "getInvolved.php", "_self", 2);
+    $Nav->addCustomNav("Downloads", "downloads.php");
+$Nav->addNavSeparator("Projects", "index.php");
+	$Nav->addCustomNav("Autotools", "/linuxtools/projectPages/autotools", "_self", 2);
+	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;ChangeLog", "/linuxtools/projectPages/changelog", "_self", 2);
+	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;eclipse-build", "/linuxtools/projectPages/eclipse-build", "_self", 2);
+	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;OProfile", "/linuxtools/projectPages/oprofile", "_self", 2);
+	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;RPM Stubby", "/linuxtools/projectPages/rpmstubby", "_self", 2);
+	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;Specfile Editor", "/linuxtools/projectPages/specfile", "_self", 2);
+	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;Systemtap", "/linuxtools/projectPages/systemtap", "_self", 2);
+	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;Valgrind", "/linuxtools/projectPages/valgrind", "_self", 2);
 $Nav->addCustomNav("Distribution Packaging Status", 	"distroStatus.php");
 ?>
