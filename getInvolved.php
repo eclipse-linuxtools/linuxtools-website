54cf386b4204d34217bf85f437d456276a900ddf
ebaron <ebaron>
2008-12-18 21:59:43 +0000

    Closed a couple divs

--- getInvolved.php
+++ getInvolved.php
@@ -78,7 +78,9 @@
 		<div class="sideitem">
 	   <h6>Incubation</h6>
 	   <div style="text-align: center">
-	   <a href="/projects/what-is-incubation.php"><img src="/images/egg-incubation.png" alt="Incubation"/></a>
+	    <a href="/projects/what-is-incubation.php"><img src="/images/egg-incubation.png" alt="Incubation"/></a>
+     </div>
+    </div>
 	</div>
 
 EOHTML;
