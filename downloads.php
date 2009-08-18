ea113a5a7d4ef2cde92d68e45b5775e1060e2e80
aoverholt <aoverholt>
2009-08-18 18:28:33 +0000

    Add commented-out information about 0.3 download.  Be sure to use mirror-friendly URLs.

--- downloads.php
+++ downloads.php
@@ -10,36 +10,43 @@
 
 	<div id="midcolumn">
 		<h1>$pageTitle</h1>
-		
-		<p> 
-		The preferred method of installation is via the 
+
+		<p>
+		The preferred method of installation is via the
 		<a href="http://wiki.eclipse.org/Linux_Tools_Project/User_Guide#Installation">Update Manager (instructions available)</a>.
+		<!--
+		Update Manager (<a href="http://wiki.eclipse.org/Linux_Tools_Project/PluginInstallHelp">instructions</a>).
+		-->
 		</p>
-		
+
 		<div class="homeitem3col" style="margin-left:40px; width:90%">
 		<!--
-		<h3>0.3.0 Release&nbsp;<a title="More Info" href="http://wiki.eclipse.org/Linux_Tools_Project/PluginInstallHelp"><img border=0 src="/linuxtools/images/icon-help.gif"></a></h3> 
+		<h3>0.3.0 Release&nbsp;<a title="More Info" href="http://wiki.eclipse.org/Linux_Tools_Project/PluginInstallHelp"><img border=0 src="/linuxtools/images/icon-help.gif"></a></h3>
 		<br>&nbsp;&nbsp;&nbsp;<a href="http://www.eclipse.org/linuxtools/new">New & Noteworthy</a> (Released 2009-08-19).
 		<br>&nbsp;&nbsp;&nbsp;Requires JRE >= 5.0 and Eclipse 3.5.
 		-->
-		<h3>0.2.1 Release&nbsp;<a title="More Info" href="http://wiki.eclipse.org/Linux_Tools_Project/User_Guide#Installation"><img border=0 src="/linuxtools/images/icon-help.gif"></a></h3> 
+		<h3>0.2.1 Release&nbsp;<a title="More Info" href="http://wiki.eclipse.org/Linux_Tools_Project/User_Guide#Installation"><img border=0 src="/linuxtools/images/icon-help.gif"></a></h3>
 		<br>&nbsp;&nbsp;&nbsp;<a href="http://www.eclipse.org/linuxtools/new">New & Noteworthy</a> (Released 2009-06-05).
 		<br>&nbsp;&nbsp;&nbsp;Requires JRE >= 5.0 and Eclipse 3.4.
 		<br>&nbsp;&nbsp;&nbsp;Specific tools require their underlying native tools (ex. GNU Autotools, Valgrind, OProfile).
 		<br>&nbsp;&nbsp;&nbsp;Other dependencies of some tools include <a href="http://eclipse.org/cdt">CDT</a> and <a href="http://eclipse.org/birt">BIRT</a>.
 		<br>&nbsp;&nbsp;&nbsp;Installations via the update site will automatically get these dependencies.
 		<!--
-		<br>&nbsp;&nbsp;&nbsp;<b>New!</b> <a href="http://download.eclipse.org/technology/linuxtools/eclipse-build/">Tarballs</a> and <a href="http://wiki.eclipse.org/Linux_Tools_Project/Eclipse_Build">instructions</a> for building the Eclipse SDK.
+		<br>&nbsp;&nbsp;&nbsp;<b>New!</b> <a href="http://www.eclipse.org/downloads/download.php?file=/technology/linuxtools/eclipse-build">Tarballs</a> and <a href="http://wiki.eclipse.org/Linux_Tools_Project/Eclipse_Build">instructions</a> for building the Eclipse SDK.
 		-->
-		<ul> 
+		<ul>
 			<li>
 				<a title="Update Site Zip" href="http://www.eclipse.org/downloads/download.php?file=/technology/linuxtools/S200905251646/linuxtools-Update-incubation-0.2.1.zip">
 					<img border=0 src="/linuxtools/images/icon-save.gif"></a>
+				<!--
+				<a title="Update Site Zip" href="http://www.eclipse.org/downloads/download.php?file=/technology/linuxtools/S200908180852/linuxtools-Update-incubation-0.3.0.zip">
+					<img border=0 src="/linuxtools/images/icon-save.gif"></a>
+				-->
 				&nbsp;&nbsp;<b>http://download.eclipse.org/technology/linuxtools/update</b>
 			</li>
 		</ul>
 		</div>
-		
+
 		<div class="homeitem3col" style="margin-left:40px; width:90%">
 		<h3>Screencast videos</h3>
 		<ul>
@@ -52,7 +59,7 @@
 		</ul>
 		</div>
 	</div>
-	
+
 	<div id="rightcolumn">
 		<div class="sideitem">
 	   <h6>Incubation</h6>
