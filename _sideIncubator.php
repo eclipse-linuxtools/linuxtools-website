724991b0210662f291441bcd3297114fd71640ae
akurtakov <akurtakov>
2010-11-04 20:37:51 +0000

    Fix missing closing tag.

--- _sideIncubator.php
+++ _sideIncubator.php
@@ -1,6 +1,7 @@
 <div class="sideitem">
-	   <h6>Incubation</h6>
-	   <div style="text-align: center">
+    <h6>Incubation</h6>
+	<div style="text-align: center">
 	    <a href="/projects/what-is-incubation.php"><img src="/images/egg-incubation.png" alt="Incubation"/></a>
+    </div>
 </div>
 	
