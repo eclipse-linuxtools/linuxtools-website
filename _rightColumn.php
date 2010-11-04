90e5986950adea6f91497e673c0b13d491bdabf0
akurtakov <akurtakov>
2010-11-04 20:06:38 +0000

    Add the incubation egg.

--- _rightColumn.php
+++ _rightColumn.php
@@ -1,4 +1,4 @@
 <div id="rightcolumn">
-	<?php //include("_sideIncubator.php"); ?>
+	<?php include("_sideIncubator.php"); ?>
 	<?php include("_news.php"); ?>
 </div>
---
+++ _sideIncubator.php
@@ -0,0 +1,6 @@
+<div class="sideitem">
+	   <h6>Incubation</h6>
+	   <div style="text-align: center">
+	    <a href="/projects/what-is-incubation.php"><img src="/images/egg-incubation.png" alt="Incubation"/></a>
+</div>
+	
