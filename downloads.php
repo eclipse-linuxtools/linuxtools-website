b623d6b539cfd789208068027cce355b28091e70
jjohnston <jjohnston>
2011-09-27 00:12:02 +0000

    Fix downloads page for 0.8.1.

--- downloads.php
+++ downloads.php
@@ -19,8 +19,8 @@
 
 		<div class="homeitem3col" style="margin-left:40px; width:90%">
 		<h3>0.8.1 Release&nbsp;<a title="More Info" href="http://wiki.eclipse.org/Linux_Tools_Project/PluginInstallHelp"><img border=0 src="/linuxtools/images/icon-help.gif"></a></h3>
-		<br>&nbsp;&nbsp;&nbsp;<a href="http://www.eclipse.org/linuxtools/new">New & Noteworthy</a> (Released 2011-06-22).
-		<a href="https://hudson.eclipse.org/hudson/job/linuxtools-Indigo/346/testReport/">Unit test results</a>.
+		<br>&nbsp;&nbsp;&nbsp;<a href="http://www.eclipse.org/linuxtools/new">New & Noteworthy</a> (Released 2011-09-26).
+		<a href="https://hudson.eclipse.org/hudson/job/linuxtools-Indigo/374/testReport/">Unit test results</a>.
 		<br>&nbsp;&nbsp;&nbsp;Requires JRE >= 6.0 and Eclipse 3.7.x.
 		<br>&nbsp;&nbsp;&nbsp;Specific tools require their underlying native tools (ex. GNU Autotools, Valgrind, OProfile, SystemTap, LTTng).
 		<br>&nbsp;&nbsp;&nbsp;Other dependencies of some tools include <a href="http://eclipse.org/cdt">CDT</a> and <a href="http://eclipse.org/birt">BIRT</a>.
