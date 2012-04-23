84069d02392177326448ae4ed50c157b6119d9a1
akurtakov <akurtakov>
2012-04-23 23:12:13 +0000

    Add more links.

--- _projectCommon.php
+++ _projectCommon.php
@@ -28,7 +28,12 @@ $pageKeywords = isset($pageKeywords) ? $pageKeywords : "linux, eclipse";
 
 # Page-specific Nav bars go here
 # Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
-$Nav->addNavSeparator("Linux Tools", "/linuxtools/index.php");
+    $Nav->addNavSeparator("Linux Tools", "/linuxtools/index.php");
+ 	$Nav->addCustomNav("Download", "/linuxtools/download.php", "_self", 3);
+	$Nav->addCustomNav("Documentation", "http://wiki.eclipse.org/Linux_Tools_Project", "_blank", 3);
+	$Nav->addCustomNav("Support", "/linuxtools/contact.php", "_blank", 3);
+	$Nav->addCustomNav("Getting Involved", "http://wiki.eclipse.org/Linux_Tools_Project/Getting_Involved", "_blank", 3);
+	
     $Nav->addCustomNav("About This Project", "/projects/project_summary.php?projectid=technology.linux-distros", "", 1);
     $Nav->addCustomNav("Plan", "http://www.eclipse.org/projects/project-plan.php?projectid=technology.linux-distros", "", 2);
     $Nav->addCustomNav("Wiki", "http://wiki.eclipse.org/index.php/Linux_Tools_Project", "_self", 2);
