13bd6243ffe6ff4e2cc1773b47ff87b1ae929669
akurtakov <akurtakov>
2012-04-24 07:28:41 +0000

    Restore original view.

--- _projectCommon.php
+++ _projectCommon.php
@@ -2,9 +2,52 @@
 
 $theme="Nova";
 
+$Nav->setLinkList(null);
+
+	$branding = <<<EOBRANDING
+<div id="branding">
+<center>
+<h2>Linux Tools Project</h2>
+</center>
+</div>
+EOBRANDING;
+
+	$Menu->setProjectBranding($branding);
+
+$pageAuthor = isset($pageAuthor) ? $pageAuthor : "";
+$pageKeywords = isset($pageKeywords) ? $pageKeywords : "linux, eclipse";
+
 # Page-specific Nav bars go here
 # Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
+$Nav->addNavSeparator("Linux Tools", "/linuxtools/index.php");
+# Break the navigation into sections
+	$Nav->addCustomNav("Download", "/tycho/download.php", "_self", 3);
+	$Nav->addCustomNav("Documentation", "/tycho/documentation.php", "_blank", 3);
+	$Nav->addCustomNav("Support", "/tycho/support.php", "_blank", 3);
+	$Nav->addCustomNav("Getting Involved", "/tycho/getting_involved.php", "_blank", 3);
 	
-   
-	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/linuxtools/style.css"/>');
+    $Nav->addCustomNav("About This Project", "/projects/project_summary.php?projectid=technology.linux-distros", "", 1);
+    $Nav->addCustomNav("Plan", "http://www.eclipse.org/projects/project-plan.php?projectid=technology.linux-distros", "", 2);
+    $Nav->addCustomNav("Wiki", "http://wiki.eclipse.org/index.php/Linux_Tools_Project", "_self", 2);
+    $Nav->addCustomNav("Contribute/Get Involved", "http://wiki.eclipse.org/Linux_Tools_Project/Getting_Involved", "_self", 2);
+    $Nav->addCustomNav("Contact Us", "/linuxtools/contact.php", "_self", 2);
+    $Nav->addCustomNav("Downloads", "/linuxtools/downloads.php");
+$Nav->addNavSeparator("C/C++ Tools Projects", "/linuxtools/index.php");
+	$Nav->addCustomNav("Autotools", "/linuxtools/projectPages/autotools", "_self", 2);
+	$Nav->addCustomNav("Callgraph", "/linuxtools/projectPages/callgraph", "_self", 2);
+	$Nav->addCustomNav("ChangeLog", "/linuxtools/projectPages/changelog", "_self", 2);
+	$Nav->addCustomNav("GProf", "/linuxtools/projectPages/gprof", "_self", 2);
+	$Nav->addCustomNav("Gcov", "/linuxtools/projectPages/gcov", "_self", 2);
+	$Nav->addCustomNav("Libhover", "/linuxtools/projectPages/libhover", "_self", 2);
+	$Nav->addCustomNav("Man Page", "/linuxtools/projectPages/manpage", "_self", 2);
+	$Nav->addCustomNav("LTTng", "/linuxtools/projectPages/lttng", "_self", 2);	
+	$Nav->addCustomNav("OProfile", "/linuxtools/projectPages/oprofile", "_self", 2);
+	$Nav->addCustomNav("Systemtap", "/linuxtools/projectPages/systemtap", "_self", 2);
+	$Nav->addCustomNav("Valgrind", "/linuxtools/projectPages/valgrind", "_self", 2);
+$Nav->addNavSeparator("Packaging/Distribution Projects", "/linuxtools/index.php");
+	$Nav->addCustomNav("eclipse-build", "http://wiki.eclipse.org/Linux_Tools_Project/Eclipse_Build", "_self", 2);
+	$Nav->addCustomNav("RPM Stubby", "/linuxtools/projectPages/rpmstubby", "_self", 2);
+	$Nav->addCustomNav("Specfile Editor", "/linuxtools/projectPages/specfile", "_self", 2);
+	$Nav->addCustomNav("Distribution Packaging Status", 	"http://wiki.eclipse.org/Linux_Distributions_Packaging_Status", "_self", 2);
+//	$App->AddExtraHtmlHeader("<link rel=\"stylesheet\" type=\"text/css\" href=\"/linuxtools/style.css\"/>\n");
 ?>
--- index.php
+++ index.php
@@ -1,21 +1,17 @@
-<?php  																														
-    require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 
 	$pageTitle 		= "Linux Tools Project";
 	$pageKeywords	= "Eclipse, Linux, autoconf, automake, autotools, CDT, OProfile, Valgrind, RPM, systemtap";
 	$pageAuthor		= "Andrew Overholt";
 
 	# Paste your HTML content between the EOHTML markers!
-	ob_start();
-	?>
-	
-
+	ob_start();?>
 <div id="bigbuttons">
 <h3>Primary Links</h3>
 <ul>
-	<li><a id="buttonDownload" href="downloads.php" title="Download">Eclipse
+	<li><a id="buttonDownload" href="download.php" title="Download">Eclipse
 	Distribution, Update Site, Dropins</a></li>
-	<li><a id="buttonDocumentation" href="http://wiki.eclipse.org/Linux_Tools_Project"
+	<li><a id="buttonDocumentation" href="documentation.php"
 		title="Documentation">Tutorials, Examples, Videos, Reference Documentation</a></li>
 	<li><a id="buttonSupport" href="contact.php" title="Support">Bug
 	Tracker, Newsgroup, Support</a></li>
@@ -23,8 +19,8 @@
 </ul>
 </div>
 
-	<div id="midcolumn" >
-	<div id="introText">
+<div id="maincontent">
+	<div id="midcolumn" class="homeitem">
 	          <img alt="Screenshot of Linux Tools projects" border="0" src="images/mainscreenshot.png">
 	          <p>
 	          The Linux Tools project aims to bring a <b>full-featured C and C++ IDE</b> to Linux developers.
@@ -42,9 +38,8 @@
 	          Eclipse SDK that can be used by all Linux distributions building and distributing it.
 	          </p>
 	</div>
-	</div>
 	    <?php include("_rightColumn.php"); ?>
-
+</div>
 <?php
     $html = ob_get_clean();
 	# Generate the web page
