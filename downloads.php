d5f09170b7aca17f150518330aeb4f4065adcd9d
akurtakov <akurtakov>
2012-06-27 14:31:36 +0000

    Proper release version.

--- downloads.php
+++ downloads.php
@@ -18,7 +18,7 @@
 		</p>
 
 		<div class="homeitem3col" style="margin-left:40px; width:90%">
-		<h3>0.10.0 Release&nbsp;<a title="More Info" href="http://wiki.eclipse.org/Linux_Tools_Project/PluginInstallHelp"><img border=0 src="/linuxtools/images/icon-help.gif"></a></h3>
+		<h3>1.0 Release&nbsp;<a title="More Info" href="http://wiki.eclipse.org/Linux_Tools_Project/PluginInstallHelp"><img border=0 src="/linuxtools/images/icon-help.gif"></a></h3>
 		<br>&nbsp;&nbsp;&nbsp;<a href="http://www.eclipse.org/linuxtools/new-1.0">New & Noteworthy</a> (Released 2012-06-27).
 		<a href="https://hudson.eclipse.org/hudson/job/linuxtools-Indigo/424/testReport/">Unit test results</a>.
 		<br>&nbsp;&nbsp;&nbsp;Requires JRE >= 6.0 and Eclipse 3.7.x.
