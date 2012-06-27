4983dd88364f320999b901dbc70b49c01996e40f
jjohnston <jjohnston>
2012-06-27 21:37:25 +0000

    Add notice that Autotools has moved to CDT and remove rest of Project Pages page.

--- projectPages/autotools/index.php
+++ projectPages/autotools/index.php
@@ -18,78 +18,11 @@
 	<div id="midcolumn">
 		<h1>$pageTitle</h1>
 
-		<h2>Overview</h2>
+		<h2>Notice</h2>
 		<p>
-		The Linux Distributions Autotools suite of plugins add support to the CDT (C/C++ Development Tools) for building and maintaining C/C++ projects that use autotools.
-Using autotools is very common in the world of Open Source projects such as those hosted at http://www.gnu.org.  With this additional support, a vast repository of C/C++ code can be checked out, built, and maintained under the CDT rather easily without having to resort to the command line.
+		With release 1.0 of Linux Tools, the Autotools plug-ins have been moved from the Linux Tools project into the CDT (C/C++ Development Tooling) project.  The plug-ins and features are now shipped as part of the CDT.
 		</p>
 
-		<h2>Current Status</h2>
-		<p>
-		In conjunction with the CDT, the plugin can do the following:
-		<ul>
-          <li>Build a C/C++ project that uses a configure script or an autogen script or a Makefile.cvs script</li>
-	  <li>Create a simple C or C++ hello world Autotools project via a template</li>
-          <li>Allow configuration parameters to be set via a gui and to rebuild when configuration parameters are changed</li>
-          <li>Colorized editing of configuration files such as configure.in, configure.ac, Makefile.am, and Makefile.in with full hover help for autoconf/automake macros</li>
-          <li>Allow specification of multiple build configurations for a single project</li>
-          <li>Run autotools such as autoconf, automake, or aclocal directly using a gui interface</li>
-	  <li>Help for autoconf and automake</li>
-        </ul>
-		</p>
-
-		<h2>Future Plans</h2>
-		<p>
-		<ul>
-		  <li>Wizard support for creation of Autotool projects</li>
-		  <li>Templates for autoconf/automake tasks</li>
-          <li>Better error parsing of autoconf/automake input files</li>
-	  <li>Support loading of missing packages noted by pkg-config</li>
-          <li>Increased test coverage</li>
-        </ul>
-		</p>
-
-		<h2>Screenshots</h2>
-		<p>
-		  <center>
-		    <img src="images/autotoolsBuild.png">
-		    <br>
-		    Autotools build
-		  </center>
-		</p>
-		<p>
-		  <center>
-		    <img src="images/autotoolsConfig2.png">
-		    <br>
-		    Autotools configuration parameters
-		  </center>
-		</p>
-		<p>
-		  <center>
-		    <img src="images/autotoolsEditor.png">
-		    <br>
-		    Autotools editor
-		  </center>
-		</p>
-
-		<h2>Try it out</h2>
-		<p>
-		  Please use our update site as described <a href="http://wiki.eclipse.org/Linux_Tools_Project/PluginInstallHelp">here</a>. <br>
-		  Or get the project plugins out of Git
-		  directly from eclipse.org at git://git.eclipse.org/gitroot/linuxtools/org.eclipse.linuxtools.git
-          You will need to check out the following from the autotools subfolder:
-<pre>
-     org.eclipse.linuxtools.cdt.autotools
-     org.eclipse.linuxtools.cdt.autotools.core
-     org.eclipse.linuxtools.cdt.autotools.ui
-     org.eclipse.linuxtools.cdt.autotools-docs
-     org.eclipse.linuxtools.cdt.autotools-feature
-</pre>
-From there, run the plugins as an Eclipse Application.
-		</p>
-	</div>
-
-	<div id="rightcolumn">
 	</div>
 
 EOHTML;
