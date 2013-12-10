c203b094cea5828baf72fb3d2156bb4e8023e407
Jeff Johnston <jjohnstn@redhat.com>
2013-12-10 13:36:03 -0500

    Linux Tools 2.2.0 release announcement.

--- _news.php
+++ _news.php
@@ -1,6 +1,8 @@
 <div class="sideitem">
 	<h6>News</h6>
 	<ul>
+		<li><em>2013-12-10</em>: Our 2.2 release is <a
+			href="http://www.eclipse.org/linuxtools/downloads.php">available</a>!</li>
 		<li><em>2013-09-27</em>: Our 2.1 release is <a
 			href="http://www.eclipse.org/linuxtools/downloads.php">available</a>!</li>
 		<li><em>2013-06-26</em>: Our 2.0 release is <a
--- downloads.php
+++ downloads.php
@@ -18,8 +18,8 @@
 		</p>
 
 		<div class="homeitem3col" style="margin-left:40px; width:90%">
-		<h3>2.1.0 Release&nbsp;<a title="More Info" href="http://wiki.eclipse.org/Linux_Tools_Project/PluginInstallHelp"><img src="/linuxtools/images/icon-help.gif" alt="Help icon" /></a></h3>
-		<br/>&nbsp;&nbsp;&nbsp;<a href="http://www.eclipse.org/linuxtools/new/2.1">New &amp; Noteworthy</a> (Released 2013-09-27).
+		<h3>2.2.0 Release&nbsp;<a title="More Info" href="http://wiki.eclipse.org/Linux_Tools_Project/PluginInstallHelp"><img src="/linuxtools/images/icon-help.gif" alt="Help icon" /></a></h3>
+		<br/>&nbsp;&nbsp;&nbsp;<a href="http://www.eclipse.org/linuxtools/new/2.2">New &amp; Noteworthy</a> (Released 2013-12-10).
 		<a href="https://hudson.eclipse.org/hudson/job/linuxtools-kepler/36/testReport/">Unit test results</a>.
 		<br/>&nbsp;&nbsp;&nbsp;Requires:
 		<ul>
@@ -34,13 +34,13 @@
 		</ul>
 		<br/>&nbsp;&nbsp;&nbsp;Installations via the Kepler p2 aggregate repository will automatically get these dependencies.
 		<br/>&nbsp;&nbsp;&nbsp;Building the Eclipse SDK?  <a href="http://download.eclipse.org/linuxtools/eclipse-build/">Tarballs</a> and <a href="http://wiki.eclipse.org/Linux_Tools_Project/Eclipse_Build">instructions</a>.
-		<br/>&nbsp;&nbsp;&nbsp;<a href="http://download.eclipse.org/linuxtools/2.1.0-sources/">Source tarballs</a> for Linux Tools plugins.
+		<br/>&nbsp;&nbsp;&nbsp;<a href="http://download.eclipse.org/linuxtools/2.2.0-sources/">Source tarballs</a> for Linux Tools plugins.
 		<ul>
 			<li>
-				<a title="p2 Repository Zip" href="http://www.eclipse.org/downloads/download.php?file=/linuxtools/linuxtools-2.1.0.zip">
+				<a title="p2 Repository Zip" href="http://www.eclipse.org/downloads/download.php?file=/linuxtools/linuxtools-2.2.0.zip">
 					<img src="/linuxtools/images/icon-save.gif" alt="Download icon"/></a>
-				&nbsp;&nbsp;(<a href="http://download.eclipse.org/linuxtools/linuxtools-2.1.0.zip.md5">md5sum</a>)
-				<b>http://download.eclipse.org/linuxtools/update-2.1</b>
+				&nbsp;&nbsp;(<a href="http://download.eclipse.org/linuxtools/linuxtools-2.2.0.zip.md5">md5sum</a>)
+				<b>http://download.eclipse.org/linuxtools/update-2.2</b>
 			</li>
 		</ul>
 		</div>
