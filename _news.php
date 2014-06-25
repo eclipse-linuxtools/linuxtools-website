6b1db73f384be8530bf001a82904888203f11310
Jeff Johnston <jjohnstn@redhat.com>
2014-06-25 14:42:45 -0400

    Update for Linux Tools 3.0 release.

--- _news.php
+++ _news.php
@@ -1,6 +1,8 @@
 <div class="sideitem">
 	<h6>News</h6>
 	<ul>
+		<li><em>2014-06-25</em>: Our 3.0 release is <a
+			href="http://www.eclipse.org/linuxtools/downloads.php">available</a>!</li>
 		<li><em>2014-03-04</em>: Our 2.2.1 release is <a
 			href="http://www.eclipse.org/linuxtools/downloads.php">available</a>!</li>
 		<li><em>2013-12-10</em>: Our 2.2 release is <a
--- downloads.php
+++ downloads.php
@@ -18,29 +18,29 @@
 		</p>
 
 		<div class="homeitem3col" style="margin-left:40px; width:90%">
-		<h3>2.2.1 Release&nbsp;<a title="More Info" href="http://wiki.eclipse.org/Linux_Tools_Project/PluginInstallHelp"><img src="/linuxtools/images/icon-help.gif" alt="Help icon" /></a></h3>
-		<br/>&nbsp;&nbsp;&nbsp;<a href="http://www.eclipse.org/linuxtools/new/2.2.1">New &amp; Noteworthy</a> (Released 2014-02-28).
-		<a href="https://hudson.eclipse.org/linuxtools/job/linuxtools-kepler/60/testReport/">Unit test results</a>.
+		<h3>3.0 Release&nbsp;<a title="More Info" href="http://wiki.eclipse.org/Linux_Tools_Project/PluginInstallHelp"><img src="/linuxtools/images/icon-help.gif" alt="Help icon" /></a></h3>
+		<br/>&nbsp;&nbsp;&nbsp;<a href="https://wiki.eclipse.org/Linux_Tools_Project/News/NewIn30">New &amp; Noteworthy</a> (Released 2014-06-25).
+		<a href="https://hudson.eclipse.org/linuxtools/job/linuxtools-luna/23/testReport/">Unit test results</a>.
 		<br/>&nbsp;&nbsp;&nbsp;Requires:
 		<ul>
-		<li>JRE >= 6.0</li>
-	        <li>Eclipse >= 4.3</li>
-		<li><a href="http://eclipse.org/cdt">CDT</a> >= 8.2.1</li>
+		<li>JRE >= 7.0</li>
+	        <li>Eclipse >= 4.4</li>
+		<li><a href="http://eclipse.org/cdt">CDT</a> >= 8.4</li>
                 <li>PTP >= 7.0 (for RDT used by remote profiling tool features)</li>
 		<li>RSE >= 3.5 (for LTTng and remote profiling tool features)</li>
 		<li>GEF >= 3.8.2 (for Draw2d and Zest packages)</li>
 		<li>Specific tools require their underlying native tools (ex. Valgrind, OProfile, SystemTap, LTTng, Perf)</li>
 		<li>There are some Orbit dependencies such as antlr</li>
 		</ul>
-		<br/>&nbsp;&nbsp;&nbsp;Installations via the Kepler p2 aggregate repository will automatically get these dependencies.
+		<br/>&nbsp;&nbsp;&nbsp;Installations via the Luna p2 aggregate repository will automatically get these dependencies.
 		<br/>&nbsp;&nbsp;&nbsp;Building the Eclipse SDK?  <a href="http://download.eclipse.org/linuxtools/eclipse-build/">Tarballs</a> and <a href="http://wiki.eclipse.org/Linux_Tools_Project/Eclipse_Build">instructions</a>.
-		<br/>&nbsp;&nbsp;&nbsp;<a href="http://git.eclipse.org/c/linuxtools/org.eclipse.linuxtools.git/tag/?id=v2.2.1">Source tarballs</a> for Linux Tools plugins.
+		<br/>&nbsp;&nbsp;&nbsp;<a href="http://git.eclipse.org/c/linuxtools/org.eclipse.linuxtools.git/tag/?id=v3.0.0">Source tarballs</a> for Linux Tools plugins.
 		<ul>
 			<li>
-				<a title="p2 Repository Zip" href="http://www.eclipse.org/downloads/download.php?file=/linuxtools/linuxtools-2.2.1.zip">
+				<a title="p2 Repository Zip" href="http://www.eclipse.org/downloads/download.php?file=/linuxtools/linuxtools-3.0.0.zip">
 					<img src="/linuxtools/images/icon-save.gif" alt="Download icon"/></a>
-				&nbsp;&nbsp;(<a href="http://download.eclipse.org/linuxtools/linuxtools-2.2.1.zip.md5">md5sum</a>)
-				<b>http://download.eclipse.org/linuxtools/update-2.2.1</b>
+				&nbsp;&nbsp;(<a href="http://download.eclipse.org/linuxtools/linuxtools-3.0.0.zip.md5">md5sum</a>)
+				<b>http://download.eclipse.org/linuxtools/update-3.0</b>
 			</li>
 		</ul>
 		</div>
