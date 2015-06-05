a231034d43620fb8b5eef01ae4a85f98983662ff
Alexander Kurtakov <akurtako@redhat.com>
2015-06-05 13:34:55 +0300

    Drop "old" news.
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- _news.php
+++ _news.php
@@ -15,30 +15,5 @@
 			href="http://www.eclipse.org/linuxtools/downloads.php">available</a>!</li>
 		<li><em>2013-06-26</em>: Our 2.0 release is <a
 			href="http://www.eclipse.org/linuxtools/downloads.php">available</a>!</li>
-		<li><em>2013-03-01</em>: Our 1.2.1 release is <a
-			href="http://www.eclipse.org/linuxtools/downloads.php">available</a>!</li>
-		<li><em>2012-11-20</em>: Our 1.2 release is <a
-			href="http://www.eclipse.org/linuxtools/downloads.php">available</a>!</li>
-		<li><em>2012-09-28</em>: Our 1.1.1 release is <a
-			href="http://www.eclipse.org/linuxtools/downloads.php">available</a>!</li>
-		<li><em>2012-07-25</em>: Our 1.1 release is <a
-			href="http://www.eclipse.org/linuxtools/downloads.php">available</a>!</li>
-		<li><em>2012-06-27</em>: Our 1.0 release is <a
-			href="http://www.eclipse.org/linuxtools/downloads.php">available</a>!</li>
-		<li><em>2012-03-23</em>: Our 0.10 release is <a
-			href="http://www.eclipse.org/linuxtools/downloads.php">available</a>!</li>
-		<li><em>2012-02-16</em>: Our 0.9.2 release is <a
-			href="http://www.eclipse.org/linuxtools/downloads.php">available</a>!</li>
-		<li><em>2012-01-20</em>: Our 0.9.1 release is <a
-			href="http://www.eclipse.org/linuxtools/downloads.php">available</a>!</li>
-		<li><em>2011-11-06</em>: Our 0.9.0 release is <a
-			href="http://www.eclipse.org/linuxtools/downloads.php">available</a>!</li>
-		<li><em>2011-09-26</em>: Our 0.8.1 release is <a
-			href="http://www.eclipse.org/linuxtools/downloads.php">available</a>
-			and is a part of <a href="http://eclipse.org/indigo/">Indigo</a>!
-			There is also an <a href="http://www.eclipse.org/downloads/">EPP
-				package available</a> for download which includes our plugins. Look
-			for "Linux Developers" on the <a
-			href="http://www.eclipse.org/downloads/">main Eclipse download page</a>.</li>
 	</ul>
 </div>
