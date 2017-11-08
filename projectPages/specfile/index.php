280d402cf44415e0207be514d805624dd8823235
Alexander Kurtakov <akurtako@redhat.com>
2017-11-08 18:28:28 +0200

    Add the mistakenly removed headers.
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- projectPages/specfile/index.php
+++ projectPages/specfile/index.php
@@ -1,4 +1,13 @@
 <?php
+
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php");
+$App = new App();
+$Nav = new Nav();
+$Menu = new Menu();
+include ($App->getProjectCommon());
+
 $pageTitle = "Linux Tools - RPM integration";
 $pageKeywords = "linux, eclipse, downloads";
 $pageAuthor = "Alexander Kurtakov";
